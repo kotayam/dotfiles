fa99b277d5ce9f6b2e4b5081bc86c541926de366
kotayam <ykomako4.9@gmail.com>
2025-11-04 11:47:57 -0500

    stow lazygit

--- setup.sh
+++ setup.sh
@@ -4,8 +4,12 @@ set -e
 
 echo "Starting dotfiles setup..."
 echo "This will create symlinks in your home directory"
+echo "..."
 
 # setup neovim
 stow -R nvim
+
+# setup lazygit
+stow -R lazygit
 
 echo "Dotfiles setup complete!"

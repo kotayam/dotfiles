8b1412168ba23be3a72d9134db334090948c234e
kotayam <ykomako4.9@gmail.com>
2026-09-14 11:57:32 -0400

    update syntax

--- nvim/.config/nvim/lua/config/plugins/lsp.lua
+++ nvim/.config/nvim/lua/config/plugins/lsp.lua
@@ -22,7 +22,6 @@ return {
 	config = function()
 		require("mason").setup()
 		local mason_lspconfig = require("mason-lspconfig")
-		local lspconfig = require("lspconfig")
 
 		-- lsp servers with options
 		local lsp_servers = {
@@ -137,7 +136,7 @@ return {
 			local opts = { on_attach = on_attach }
 
 			if conf.settings then
-				opts.settings = conf.settings
+				opts.settings = type(conf.settings) == "function" and conf.settings() or conf.settings
 			end
 
 			if conf.filetypes then
@@ -148,7 +147,8 @@ return {
 				opts.cmd = conf.cmd
 			end
 
-			lspconfig[name].setup(opts)
+			vim.lsp.config(name, opts)
+			vim.lsp.enable(name)
 		end
 	end,
 }

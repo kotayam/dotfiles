8f4c3feaeefaf8d577751d87ad4691b5a0e66b9e
kotayam <ykomako4.9@gmail.com>
2026-09-18 17:43:04 -0400

    no popup for warnings when vimtex compilation

--- nvim/.config/nvim/lua/config/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/config/plugins/vimtex.lua
@@ -5,6 +5,7 @@ return {
 	init = function()
 		vim.g.vimtex_view_method = "zathura"
 		vim.g.vimtex_compiler_method = "latexmk"
+		vim.g.vimtex_quickfix_open_on_warning = 0
 		vim.g.vimtex_compiler_latexmk = {
 			continuous = 1,
 		}
